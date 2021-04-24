ec9468db4ceacb4101f50fc7d9e9a898b2149302
Tehila Naki <72945182+TehilaNaki@users.noreply.github.com>
2021-04-24 23:45:34 +0300

    level_4 add CameraTest and get/set

--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -1,6 +1,7 @@
 package elements;
 
 import primitives.Point3D;
+import primitives.Ray;
 import primitives.Vector;
 
 public class Camera  {
@@ -22,5 +23,63 @@ public class Camera  {
             throw new IllegalArgumentException("vUp Not Orthogonal to vTO");
         }
         vRight=vT0.crossProduct(vUp);
+    }
+
+    public Point3D getP0() {
+        return p0;
+    }
+
+    public Vector getvT0() {
+        return vT0;
+    }
+
+    public Vector getvUp() {
+        return vUp;
+    }
+
+    public void setWidth(double width) {
+        if(width<=0) {
+
+            throw new IllegalArgumentException("Illegal value of width");
+        }
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        if(height<=0) {
+
+            throw new IllegalArgumentException("Illegal value of width");
+        }
+        this.height = height;
+    }
+
+    public Vector getvRight() {
+        return vRight;
+    }
+    public Camera setViewPlaneSize(double _width, double _height) {
+
+        if(_width<=0) {
+
+            throw new IllegalArgumentException("Illegal value of width");
+        }
+        if(_height<=0) {
+
+            throw new IllegalArgumentException("Illegal value of height");
+        }
+        width=_width;
+        height=_height;
+        return this;
+    }
+    public Camera setDistance(double _distance){
+
+        if(_distance<=0) {
+
+            throw new IllegalArgumentException("Illegal value of distance");
+        }
+        distance=_distance;
+        return this;
+    }
+    public Ray constructRayThroughPixel(int nX, int nY, int j, int i){
+        return null;
     }
 }
